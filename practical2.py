f7fedf90229860bcdbec0baaede8f9084d580459
neshal75 <02240086.cst@rub.edu.bt>
2025-04-16 13:31:55 +0600

    Add files via upload

---
+++ practical2.py
@@ -0,0 +1,10 @@
+my_array = [42, "hello", 8.21, True, None]
+firstArrayLength = len(my_array)
+
+print(len(my_array))
+
+my_array.append("value")
+
+secondArrayLength = len(my_array)
+
+print( firstArrayLength - secondArrayLength)
---
+++ practical3.py
@@ -0,0 +1,45 @@
+country  = ["bhutan", "india", "chaina", "bangladesh"]
+
+arrayLength = len(country)
+
+for i in range(arrayLength):
+    print(country[i])
+
+
+
+
+
+
+
+
+country  = ["Bhutan", "India", "chaina", "bangladesh"]
+
+arrayLength = len(country)
+
+for index in range(arrayLength):
+    print(country[index].lower())
+
+
+
+country  = ["Bhutan", "India", "chaina", "bangladesh"]
+for element in country:
+    print(element)
+
+
+# while loop
+
+
+
+country  = ["Bhutan", "India", "chaina", "bangladesh"]
+
+arrayLen = len(country)
+index = 0
+while index < arrayLen:
+    print(country[index])
+    index = index + 1
+
+
+
+
+
+
